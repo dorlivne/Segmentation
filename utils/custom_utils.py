4fc73416d66459fc6c36d75be8ad67d84a8c048d
Dor Livne <dorlivne2308@gmail.com>
2019-07-01 19:18:55 +0300

    Add files via upload

--- utils/custom_utils.py
+++ utils/custom_utils.py
@@ -2,9 +2,15 @@ from loss import create_weights
 
 
 def extract_weight_map(queue_seg, queue_weightmap):
-    seg = queue_seg.get()
-    queue_weightmap.put(create_weights(seg=seg))
-    extract_weight_map(queue_seg, queue_weightmap)
+    while True:
+        seg = queue_seg.get()
+        try:
+            if not seg:
+                print("utility process exiting...")
+                return
+        except Exception:
+            pass
+        queue_weightmap.put(create_weights(seg=seg))
 
 
 # TODO implement this function
