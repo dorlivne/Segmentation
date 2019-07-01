48ff243ff3bba86ad72d261b022443e243d6fb60
Dor Livne <dorlivne2308@gmail.com>
2019-07-01 20:05:58 +0300

    Add files via upload

--- utils/custom_utils.py
+++ utils/custom_utils.py
@@ -1,13 +1,27 @@
-from loss import create_weights, jaccard_score, tf
-import numpy as np
+from loss import create_weights, tf, np
+#from sklearn.metrics import jaccard_score
+
 
 def extract_weight_map(queue_seg, queue_weightmap):
-    seg = queue_seg.get()
-    queue_weightmap.put(create_weights(seg=seg))
-    extract_weight_map(queue_seg, queue_weightmap)
+    while True:
+        seg = queue_seg.get()
+        try:
+            if not seg:
+                return
+        except Exception:
+            pass
+        queue_weightmap.put(create_weights(seg=seg))
+
+"""
+def Jaccard_Index2(output_batch, gt_batch, channel_axis=-1, foreground_class_index=1):
+    gt_batch = tf.squeeze(gt_batch, channel_axis)
+    gt_foreground = (tf.equal(tf.cast(gt_batch, tf.float32), foreground_class_index).numpy()).astype(np.int8)
+    output_classes = tf.argmax(output_batch, axis=channel_axis)
+    output_foreground = (tf.equal(output_classes, foreground_class_index).numpy()).astype(np.int8)
+    return jaccard_score(gt_foreground.flatten(), output_foreground.flatten()
+"""
 
 
-# TODO implement this function
 def Jaccard_Index(gt_batch, output_batch, channel_axis=-1, foreground_class_index=1):
     spatial_dimensions = (1, 2)
     gt_batch = tf.squeeze(gt_batch, channel_axis)
