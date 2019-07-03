af5d03a3d0ffefc2e81e91d405e75c37f4a9a0a1
moshekoz <49647681+moshekoz@users.noreply.github.com>
2019-07-03 11:07:38 +0300

    Add files via upload

--- utils/custom_utils.py
+++ utils/custom_utils.py
@@ -1,28 +1,15 @@
-from loss import create_weights, tf, np
-#from sklearn.metrics import jaccard_score
-
+from loss import create_weights, tf
+import numpy as np
+import matplotlib.pyplot as plt
 
 def extract_weight_map(queue_seg, queue_weightmap):
-    while True:
-        seg = queue_seg.get()
-        try:
-            if not seg:
-                return
-        except Exception:
-            pass
-        queue_weightmap.put(create_weights(seg=seg))
-
-"""
-def Jaccard_Index2(output_batch, gt_batch, channel_axis=-1, foreground_class_index=1):
-    gt_batch = tf.squeeze(gt_batch, channel_axis)
-    gt_foreground = (tf.equal(tf.cast(gt_batch, tf.float32), foreground_class_index).numpy()).astype(np.int8)
-    output_classes = tf.argmax(output_batch, axis=channel_axis)
-    output_foreground = (tf.equal(output_classes, foreground_class_index).numpy()).astype(np.int8)
-    return jaccard_score(gt_foreground.flatten(), output_foreground.flatten()
-"""
+    seg = queue_seg.get()
+    queue_weightmap.put(create_weights(seg=seg))
+    extract_weight_map(queue_seg, queue_weightmap)
 
 
-def Jaccard_Index(gt_batch, output_batch, channel_axis=-1, foreground_class_index=1):
+# TODO implement this function
+def Jaccard_Index(gt_batch, output_batch, channel_axis=-1, foreground_class_index=1,visual=False):
     spatial_dimensions = (1, 2)
     gt_batch = tf.squeeze(gt_batch, channel_axis)
     gt_foreground = (tf.equal(tf.cast(gt_batch, tf.float32), foreground_class_index).numpy()).astype(np.int8)
@@ -36,4 +23,13 @@ def Jaccard_Index(gt_batch, output_batch, channel_axis=-1, foreground_class_inde
     num_objects = tf.cast(tf.math.count_nonzero(tf.greater(area, 0)), tf.float32)
     jaccard = tf.cast(tf.divide(intersection, union + 0.00000001), tf.float32)
     seg_measure = tf.reduce_sum(jaccard)/num_objects
+    if visual:
+        plt.subplot(1, 2, 1)
+        plt.imshow(gt_foreground[0])
+        plt.colorbar()
+        plt.subplot(1, 2, 2)
+        plt.imshow(output_foreground[0])
+        plt.colorbar()
+        plt.show()
+
     return seg_measure
